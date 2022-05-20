9029b52fec20e30d5445876e09870ea83a224434
Beata VÃoelker <beataregina.voelker@gmail.com>
2022-05-20 21:45:03 +0200

    unfinishedcoaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,17 @@
 class QuestionsController < ApplicationController
   def ask
   end
+
+  def answer
+    @question = params[:question] # this will store the user input from the form in the variable @question; params collects the data from the request:
+    if @question.blank?
+      @answer = "I can't hear you!"
+    elsif @question =~ /i am going to work/i
+      @answer = "Great!"
+    elsif @question.ends_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "ask", to: "questions#ask"
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'ask', to: 'questions#ask', as: :ask # will define a route to the ask-user action
+  get 'answer', to: 'questions#answer', as: :answer # will define a route to the answer-user action on another page
 end
